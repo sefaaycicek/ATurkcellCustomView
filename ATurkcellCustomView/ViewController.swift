feef8bcfd1e2144b6e07e6f7cecf5d6dd2dfc05e
Sefa Aycicek <saycicek@ipucubilisim.com.tr>
2024-10-02 09:38:59 +0300

    Initial Commit

---
+++ ATurkcellCustomView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ATurkcellCustomView
+//
+//  Created by Sefa Aycicek on 2.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ATurkcellCustomViewUITests/ATurkcellCustomViewUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ATurkcellCustomViewUITestsLaunchTests.swift
+//  ATurkcellCustomViewUITests
+//
+//  Created by Sefa Aycicek on 2.10.2024.
+//
+
+import XCTest
+
+final class ATurkcellCustomViewUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
